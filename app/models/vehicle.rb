5d51eed66c17265f52cd3c9ad5c9aa7413b4ba90
ritorocks <ritorocks@gmail.com>
2024-08-16 18:31:47 +0000

    final

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -29,7 +29,7 @@ class Vehicle < ApplicationRecord
 
   # Direct associations
   has_many  :trips, class_name: "Trip", foreign_key: "vehicle_id"
-  has_many  :jobs, class_name: "Job", foreign_key: "vehicle_id", dependent: :destroy
+  #has_many  :jobs, class_name: "Job", foreign_key: "vehicle_id", dependent: :destroy
 
   # Indirect associations
   has_many :guests, through: :trips, source: :guest
